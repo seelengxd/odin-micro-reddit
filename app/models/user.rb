3b7dcdae72cdec299c82bb1473ad32466b5fc149
seeleng <seelengxd@gmail.com>
2021-12-11 20:04:42 +0800

    add post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { in: 6..20 }, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :posts
 end
